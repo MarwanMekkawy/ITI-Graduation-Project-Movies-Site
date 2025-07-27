1158fa94460a9adb465a9c38337c4a394f7b993c
Marwan <marwanmohamed5174@gmail.com>
2025-07-27 19:15:54 +0300

    added footer & navbar without the dropdown menu

--- Project.FrontEnd/src/app/app.ts
+++ Project.FrontEnd/src/app/app.ts
@@ -2,6 +2,9 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- Project.FrontEnd/src/app/footer/footer.ts
+++ Project.FrontEnd/src/app/footer/footer.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrls: ['./footer.css']
 })
 export class Footer {
-
+  currentYear = new Date().getFullYear();
 }
