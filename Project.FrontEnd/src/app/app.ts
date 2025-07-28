bcc25275ccc13647f01b5ebe2d45a1c36ad8f9a1
mohammed_tash <mohammedtash53@gmail.com>
2025-07-28 15:36:50 +0300

    added watchlist page and movie card styles

--- Project.FrontEnd/src/app/app.ts
+++ Project.FrontEnd/src/app/app.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { CarouselComponent } from "./child-components/carousel/carousel";
 import { VideoPlayer } from "./child-components/video-player/video-player";
 import { HomePage } from "./Page-components/home-page/home-page";
+import { WatchlistPage } from "./Page-components/watchlist-page/watchlist-page";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CarouselComponent, VideoPlayer, HomePage],
+  imports: [RouterOutlet, CarouselComponent, VideoPlayer, HomePage, WatchlistPage],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
