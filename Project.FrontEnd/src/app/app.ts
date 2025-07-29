e98140eed3ef8d35d2ddb2bbb1caf75807d7c702
Moussa <mohamedmohsen4123@gmail.com>
2025-07-29 20:24:49 +0300

    add weclome page images and update styles

--- Project.FrontEnd/src/app/app.ts
+++ Project.FrontEnd/src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginPageComponent } from "./Page-components/login-page/login-page";
-import { SignupPageComponent } from "./Page-components/signup-page/signup-pag";
+// import { LoginPageComponent } from './Page-components/login-page/login-page';
+// import { SignupPageComponent } from './Page-components/signup-page/signup-pag';
 import { ErrorPage } from './Page-components/error-page/error-page';
-
-
+import { WelcomePage } from './Page-components/welcome-page/welcome-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPageComponent, SignupPageComponent,ErrorPage],
+  imports: [RouterOutlet, WelcomePage],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
