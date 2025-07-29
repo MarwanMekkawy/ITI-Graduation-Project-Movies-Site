71e3091dd0e6562de5509f3ac77427f690ef84be
Moussa <mohamedmohsen4123@gmail.com>
2025-07-29 18:32:29 +0300

    Error Page Added

--- Project.FrontEnd/src/app/app.ts
+++ Project.FrontEnd/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from "./Page-components/login-page/login-page";
+import { SignupPageComponent } from "./Page-components/signup-page/signup-pag";
+import { ErrorPage } from './Page-components/error-page/error-page';
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPageComponent, SignupPageComponent,ErrorPage],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
