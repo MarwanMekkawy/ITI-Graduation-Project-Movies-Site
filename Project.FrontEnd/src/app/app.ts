2e09a4e84de6056e7f437529176f78753b96c125
mohammed_tash <mohammedtash53@gmail.com>
2025-07-30 12:00:05 +0300

    style edit

--- Project.FrontEnd/src/app/app.ts
+++ Project.FrontEnd/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./child-components/navbar/navbar";
+import { Footer } from "./child-components/footer/footer";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
