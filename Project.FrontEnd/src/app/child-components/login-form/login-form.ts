0d6d44231b9b85acfd5f2ab1fe17a2402cd89d73
Marwan <marwanmohamed5174@gmail.com>
2025-08-07 01:38:29 +0300

    finished signup & signin impelementation and api linking

--- Project.FrontEnd/src/app/child-components/login-form/login-form.ts
+++ Project.FrontEnd/src/app/child-components/login-form/login-form.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from './../../core/services/auth/auth-service';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -9,30 +11,31 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login-form.css'
 })
 export class LoginForm {
- login: FormGroup = new FormGroup({
- username: new FormControl(null, [Validators.required]),
- password: new FormControl(null, [Validators.required]),
- })
+  login: FormGroup = new FormGroup({
+    username: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+  })
 
-private readonly authService = inject(AuthService)
+  private readonly authService = inject(AuthService);
+  private readonly redirect = inject(Router);
+  error: string='';
 
   registerTheForm(): void {
     if (this.login.invalid) {           //applies validation
       this.login.markAllAsTouched();
-      return; 
+      return;
     }
-
 
     this.authService.sendLoginForm(this.login.value).subscribe({
       next: (res) => {
-        console.log(res);
+        if (res.token) {
+          localStorage.setItem(`token`, res.token);
+          this.redirect.navigate([`/home`]);
+        }
       },
-      error: (err) => {
-        console.log(err)
+      error: (err: HttpErrorResponse) => {
+        this.error=err.error.error;      
       }
     })
   }
-
-
 }
- 
