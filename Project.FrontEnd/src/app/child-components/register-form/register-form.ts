677ddc51a7337a24e738eee4317d2f1b01bd7189
Marwan <marwanmohamed5174@gmail.com>
2025-08-06 19:58:56 +0300

    allowed cors policy

--- Project.FrontEnd/src/app/child-components/register-form/register-form.ts
+++ Project.FrontEnd/src/app/child-components/register-form/register-form.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-form.html',
   styleUrl: './register-form.css'
 })
 export class RegisterForm {
 
+  register: FormGroup = new FormGroup({
+    username: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Za-z\d]+$/), Validators.minLength(8), Validators.maxLength(20)]),
+  });
+  registerTheForm(): void {
+    console.log(this.register)
+  }
 }
