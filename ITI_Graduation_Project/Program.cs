906a2a02a548b00b2f4290a92db224a00eafe7ef
Marwan <marwanmohamed5174@gmail.com>
2025-08-06 23:08:34 +0300

    added cors policy 2

--- ITI_Graduation_Project/Program.cs
+++ ITI_Graduation_Project/Program.cs
@@ -43,6 +43,17 @@ namespace ITI_Graduation_Project
                 };
             });
 
+            //cors policy
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -51,6 +62,8 @@ namespace ITI_Graduation_Project
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors("AllowAll");
 
             app.UseHttpsRedirection();
 
